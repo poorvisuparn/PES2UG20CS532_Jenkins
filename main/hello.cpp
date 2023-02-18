80ad6f966739972b6b43b75896d7d7f81e0b446e
Poorvi Deshpande <93137604+poorvisuparn@users.noreply.github.com>
2023-02-18 09:51:21 +0530

    updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I have made a change\n";
+  cout << "I have made a change in hello.cpp\n";
   return 0;
 }
